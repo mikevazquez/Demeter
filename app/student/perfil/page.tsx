23dd72a77a7f55f00c8dc2a4070ef1d0ab0acf1e
Marco Antonio Vazquez Nuñez <55609925+mikevazquez@users.noreply.github.com>
2026-09-23 16:25:56 -0600

    Fix avatar picker import formatting

--- app/student/perfil/page.tsx
+++ app/student/perfil/page.tsx
@@ -5,7 +5,8 @@ import { formatDate, getStudentPortalContext } from "@/lib/student/portal";
 
 import { updateStudentAvatarAction, updateStudentProfileAction } from "../actions";
 import PendingActionButton from "../components/PendingActionButton";
-import StudentNoticeDialog from "../components/StudentNoticeDialog";\nimport AvatarFilePicker from "./AvatarFilePicker";
+import StudentNoticeDialog from "../components/StudentNoticeDialog";
+import AvatarFilePicker from "./AvatarFilePicker";
 
 const errorCopy: Record<string, string> = {
   email_invalid: "Revisa el formato de tu correo.",
